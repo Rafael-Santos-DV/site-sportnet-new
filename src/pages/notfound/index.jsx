b55db84440728588f5f31f45093038775198f393
Rafael Santos <santosrafaell187@gmail.com>
2022-01-24 20:19:12 -0300

    page-not-found foi adicionada

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -1,5 +1,27 @@
+import { ContainerNotFound } from "./style";
+import { FaSearch } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 export const NotFound = () => {
   return(
-    <p>Página não encontrada</p>
+    <ContainerNotFound className="container-page-notfound">
+      <Link to="/">
+        <div>
+            <span className="number">
+              4
+            </span>
+            <span className="number">
+              0
+            </span>
+            <span className="number">
+              4
+            </span>
+            <div className="container-text-notfound">
+              <h1>Página não encontrada</h1>
+              <FaSearch className="icon"/>
+            </div>
+        </div>
+      </Link>
+    </ContainerNotFound>
   )
 };
---
+++ src/pages/notfound/style.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+
+export const ContainerNotFound = styled.div`
+  width: 100%;
+
+  > a >  div {
+    width: 100%;
+    max-width: 1100px;
+    margin: 0 auto;
+    text-align: center;
+    padding: 100px 20px;
+
+    > span {
+      display: inline-block;
+      color: #25D366;
+      font-family: 'Lobster', cursive;
+      font-size: 300px;
+      text-shadow: 4px 20px 10px #000000;
+      letter-spacing: 30px;
+
+      &:nth-of-type(1) {
+        transform: rotate(-30deg);
+
+      }
+      &:nth-of-type(2) {
+        transform: rotate(-20deg);
+
+      }
+      &:nth-of-type(3) {
+        transform: rotate(30deg);
+
+      }
+    }
+    > div.container-text-notfound {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 10px;
+
+
+      > h1 {
+        font-family: 'Lobster', cursive;
+        letter-spacing: 2px;
+        color: #cc0000;
+      }
+
+      > .icon {
+        font-size: 30px;
+        color: #25D366;
+
+      }
+
+    }
+
+    @media only screen and (max-width: 668px) {
+      > span {
+        font-size: 30vw;
+
+      }
+      > div.container-text-notfound {
+        > h1 {
+          font-size: 4vw;
+        }
+        > .icon {
+          font-size: 3.5vw;
+        }
+      }
+    }
+  }
+
+`;
